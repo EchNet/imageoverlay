76ec7b76cc8e5ed5102f60db6582e58b59dd947b
James Echmalian <ech@ech.net>
2020-10-03 00:48:20 -0400

    Implement save and restore of settings.

---
+++ settings/_default.php
@@ -0,0 +1,14 @@
+<?php 
+$margin_left = 0;
+$margin_top = 0;
+$margin_right = 0;
+$margin_bottom = 0;
+
+$xalign1 = "center";
+$yalign1 = "center";
+$fontface1 = "arial";
+$fontsize1 = 40;
+$fontcolor1 = "000000";
+
+$ntext = 3;
+?>
--- tsettings.php
+++ tsettings.php
@@ -1,7 +1,49 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <?php 
+  include "settings/_default.php";
+
+  # URI is path/to/script.php/template_name
   $template_name = substr($_SERVER["PATH_INFO"], 1);
+
+  # Settings for the identified template are expressed in a PHP settings file.
+  $settings_file_path = "settings/" . $template_name . ".php";
+
+  if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    # Write POSTed parameters to PHP settings file.
+    $settings_file_data = array("<?php\n");
+
+    # Margin fields are singleton.
+    $marginfields = array("margin_left", "margin_right", "margin_top", "margin_bottom");
+    foreach ($marginfields as $f) {
+      if (isset($_POST[$f]) && $_POST[$f] !== "") {
+        $value = $_POST[$f];
+        array_push($settings_file_data, "\$$f = \"$value\";\n");
+      }
+    }
+
+    # One set of text fields for each text area.
+    for ($t = 1; $t <= $ntext; $t++) {
+      $textfields = array("xalign", "yalign", "fontface", "fontsize", "fontcolor");
+      foreach ($textfields as $f) {
+        if (isset($_POST["$f$t"]) && $_POST["$f$t"] !== "") {
+          $value = $_POST["$f$t"];
+          array_push($settings_file_data, "\$$f$t = \"$value\";\n");
+        }
+      }
+    }
+
+    array_push($settings_file_data, "?>\n");
+
+    if (file_put_contents($settings_file_path, $settings_file_data) === false) {
+      echo("File write operation failed");
+    }
+  }
+
+  # Load PHP settings files: default and specific.
+  if (file_exists($settings_file_path)) {
+    include $settings_file_path;
+  }
 ?>
 <head>
   <title>Image Overlay - Template Settings</title>
@@ -15,28 +57,63 @@
       <img class="preview" src="../script3.php?image=<?php echo $template_name ?>.jpg" />
     </div>
     <form method="POST">
-      <div class="formField">
-        <label>Font Color:</label>
-        <input id="simple-color-picker" type="text" name="fontcolor1" value="000000" size="50" class="jscolor wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true"   aria-invalid="false" placeholder="Font Color"/>
+      <div class="flexwrap">
+        <div class="formField">
+          <label>Left Margin:</label>
+          <input type="number" name="margin_left" value="<?php echo $margin_left ?>" size="20" class="form-control" placeholder="Left Margin"/>
+        </div>
+        <div class="formField">
+          <label>Right Margin:</label>
+          <input type="number" name="margin_right" value="<?php echo $margin_right ?>" size="20" class="form-control" placeholder="Right Margin"/>
+        </div>
+        <div class="formField">
+          <label>Top Margin:</label>
+          <input type="number" name="margin_top" value="<?php echo $margin_top ?>" size="20" class="form-control" placeholder="Top Margin"/>
+        </div>
+        <div class="formField">
+          <label>Bottom Margin:</label>
+          <input type="number" name="margin_bottom" value="<?php echo $margin_bottom ?>" size="20" class="form-control" placeholder="Bottom Margin"/>
+        </div>
       </div>
-      <div class="formField">
-        <label>Font Size (8-100):</label>
-        <input type="number" name="fontsize1" value="40" size="50" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Font Size (8-100)" min="8" max="100"/>
-      </div>
-      <div class="formField">
-        <label>Left Margin:</label>
-        <input type="number" name="xmargin1" value="120" size="50" class="form-control wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true"   aria-invalid="false" placeholder="Left Margin"/>
-      </div>
-      <div class="formField">
-        <label>Top Margin:</label>
-        <input type="number" name="ymargin1" value="350" size="50" class="form-control wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Top Margin"/>
-      </div>
-      <div style="formField">
+      <?php
+        for ($t = 1; $t <= $ntext; $t++) {
+          echo '<h4>Text Area #' . $t . '</h4>';
+          echo '<div class="flexwrap">';
+          $fieldname = "fontface" . $t;
+          echo '<div class="formField">';
+          echo '<label>Font Face:</label>';
+          echo '<input type="text" name="fontface' . $t . '" value="' . $$fieldname . '" size="20" class="form-control" placeholder="Font Face"/>';
+          echo '</div>';
+          $fieldname = "fontcolor" . $t;
+          echo '<div class="formField">';
+          echo '<label>Text Color:</label>';
+          echo '<input type="text" name="fontcolor' . $t . '" value="' . $$fieldname . '" size="20" class="jscolor form-control" placeholder="Text Color"/>';
+          echo '</div>';
+          $fieldname = "fontsize" . $t;
+          echo '<div class="formField">';
+          echo '<label>Font Size:</label>';
+          echo '<input type="number" name="fontsize' . $t . '" value="' . $$fieldname . '" size="20" class="form-control" min="8" max="100"/>';
+          echo '</div>';
+          echo '</div>';
+          echo '<div class="flexwrap">';
+          $fieldname = "xalign" . $t;
+          echo '<div class="formField">';
+          echo '<label>Horizontal Alignment:</label>';
+          echo '<input type="text" name="xalign' . $t . '" value="' . $$fieldname . '" size="20" class="form-control" placeholder="left, center, or right"/>';
+          echo '</div>';
+          $fieldname = "yalign" . $t;
+          echo '<div class="formField">';
+          echo '<label>Vertical Alignment:</label>';
+          echo '<input type="text" name="yalign' . $t . '" value="' . $$fieldname . '" size="20" class="form-control" placeholder="top, center, or bottom"/>';
+          echo '</div>';
+          echo '</div>';
+        }
+      ?>
+      <div class="flexwrap">
         <button id="submit_btn" type="submit" class="wpcf7-form-control wpcf7-submit" name="submit">Save</button>
       </div>
     </form>
   </div>
 </body>
-<script src="js/jscolor.js"></script>
-<script src="js/imageoverlay.js"></script>
+<script src="../js/jscolor.js"></script>
 </html>
